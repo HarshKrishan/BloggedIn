33cfb3fab2bd76827bd467862811862ac379416e
HarshKrishan <krishanharsh82@gmail.com>
2023-07-13 00:22:50 +0530

    fourth draft

--- src/app/api/myposts/[name]/route.js
+++ src/app/api/myposts/[name]/route.js
@@ -1,4 +1,4 @@
-import user from "../models/user";
+import User from "../models/user";
 import connectMongoDB from "../libs/mongodb";
 
 import { NextResponse } from "next/server";
@@ -6,7 +6,7 @@ import { NextResponse } from "next/server";
 export async function GET(request, { params }) {
   const name = request.nextUrl.searchParams.get("name");
   await connectMongoDB();
-  const user = await user.find({ username: name });
+  const user = await User.find({ username: name });
   const posts = user.saved_posts;
   if (posts.length === 0) {
     return NextResponse.json({ message: "No posts found" }, { status: 200 });
